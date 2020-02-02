94122833d279acbf35688d45c74718a675185dca
MiSilvaSouza <mirian.s2@hotmail.com>
2020-02-02 15:21:15 -0300

    make it better

--- api/server/controllers/ProductController.js
+++ api/server/controllers/ProductController.js
@@ -3,10 +3,9 @@ import models from '../src/models/index'
 const getAllProduct = async (req, res) => {
   const products = await models.products.findAll({raw: true})
   res.json(products)
-  //console.log(products)
-
 }
 
 export default {
-  getAllProduct
+  getAllProduct,
+  
 }
